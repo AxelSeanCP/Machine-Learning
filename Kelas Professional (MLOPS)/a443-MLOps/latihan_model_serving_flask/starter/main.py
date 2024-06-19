cae97f64a2f70a46add84719dc57056d6950bf3f
Axel Sean Cahyono Putra <axelseancp@gmail.com>
2024-06-20 00:00:48 +0700

    add file

---
+++ Kelas Professional (MLOPS)/a443-MLOps/latihan_model_serving_flask/starter/main.py
@@ -0,0 +1,20 @@
+import json
+import numpy as np
+import tensorflow as tf
+from flask import Flask, request
+
+
+#================Membuat Web App Sederhana Menggunakan Flask========================
+# Your code here
+#===================================================================================
+
+
+#============Membuat API Endpoint dalam Web App sebagai Model Serving===============
+# Your code here
+#===================================================================================
+
+
+
+#=============================Menjalankan Web App===================================
+# Your code here
+#===================================================================================
