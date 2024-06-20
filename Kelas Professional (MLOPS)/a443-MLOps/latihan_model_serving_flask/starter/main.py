0d07116e7feef0e359553ea6202364f5ee7574a9
Axel Sean Cahyono Putra <axelseancp@gmail.com>
2024-06-20 23:12:25 +0700

    Latihan membuat model serving menggunakan flask

--- Kelas Professional (MLOPS)/a443-MLOps/latihan_model_serving_flask/starter/main.py
+++ Kelas Professional (MLOPS)/a443-MLOps/latihan_model_serving_flask/starter/main.py
@@ -5,16 +5,47 @@ from flask import Flask, request
 
 
 #================Membuat Web App Sederhana Menggunakan Flask========================
-# Your code here
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "Hello, World!"
 #===================================================================================
 
 
 #============Membuat API Endpoint dalam Web App sebagai Model Serving===============
-# Your code here
+MODEL_PATH = "fashion-mnist"
+model = tf.keras.models.load_model(MODEL_PATH)
+
+def data_preprocessing(image):
+    image = np.array(image) / 255.0
+    image = (np.expand_dims(image, 0))
+    return image
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    request_json = request.json
+    image = data_preprocessing(request_json.get("data"))
+
+    prediction = model.predict(image)
+    prediction = tf.argmax(prediction[0]).numpy()
+
+    class_names = [
+         'T-shirt/top', 'Trouser', 'Pullover', 'Dress',
+        'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot'
+    ]
+
+    response_json = {
+        "prediction": class_names[prediction]
+    }
+
+    return json.dumps(response_json)
+
 #===================================================================================
 
 
 
 #=============================Menjalankan Web App===================================
-# Your code here
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=5000)
 #===================================================================================
